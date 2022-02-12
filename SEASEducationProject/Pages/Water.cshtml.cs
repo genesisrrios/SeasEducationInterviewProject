41582fdedf6096da0605b85b7c580be248077316
Genesis Rivera <genesisrrios@gmail.com>
2022-02-12 18:51:49 -0400

    Water page added results property and added Error message handling. Water page added inputs fro length, width and height. Water code added for post method handling.

--- SEASEducationProject/Pages/Water.cshtml.cs
+++ SEASEducationProject/Pages/Water.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SEASEducationProject.ViewModels;
+using System.Text;
 
 namespace SEASEducationProject.Pages
 {
     public class WaterModel : PageModel
     {
+        [BindProperty]
+        public Box BoxModel { get; set; }
+        public string ErrorMessage { get; set; } = String.Empty;
+        public string Result { get; set; } = String.Empty;
         public void OnGet()
         {
+        }
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var volume = BoxModel.Volume;
+            }
+            else
+            {
+                ErrorMessage = String.Join(" ", ModelState.Values
+                                        .SelectMany(x => x.Errors)
+                                        .Select(x => x.ErrorMessage));
+            }
         }
     }
 }
---
+++ SEASEducationProject/ViewModels/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SEASEducationProject.ViewModels
+{
+    public class Box
+    {
+        [Required]
+        [Range(0, 999.99)]
+        public decimal Length { get; set; }
+        [Range(0, 999.99)]
+        [Required]
+        public decimal Width { get; set; }
+        [Required]
+        [Range(0, 999.99)]
+        public decimal Height { get; set; }
+
+        public decimal Volume
+        { 
+            get 
+            {
+                return Length * Width * Height;
+            } 
+        }
+    }
+}
